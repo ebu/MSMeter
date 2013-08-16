8a8804c46282ffa98930371b25b94a9ae83489ad
Vokatch <igor.vokatch-boldyrev@epfl.ch>
2013-08-16 15:24:33 +0200

    Java parser for csv files
    uploaded stable version of a Java parser:
    -reads data from the csv (which is not even a "comma separated value
    file anymore" generated by CtrlApp (data formatting not perfect for
    excel, R or gnuplot "reading")
    -generates a real "csv", with every line giving infos on a frame
    read/write
    -The times (which were in seconds+microseconds) from 1/1/1900 had too
    big values (had to use the BigInteger class- not acceptable for excel
    and other tools): the times are recalculated as dependent on the
    beginning of the session (t=0)

---
+++ meterParseJava/MeterOutput.java
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class MeterOutput {
+
+	
+	private BufferedWriter bw;
+	private FileWriter fw;
+	
+	public MeterOutput(String filename) throws IOException {
+		fw = new FileWriter(filename);
+		bw = new BufferedWriter (fw);
+	}
+	
+	public void write(String line) throws IOException{
+		bw.write(line);
+		bw.newLine();
+		
+	}
+	
+	public void close(){
+		try {
+			bw.close();
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
